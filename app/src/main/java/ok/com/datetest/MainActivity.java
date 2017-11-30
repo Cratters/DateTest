22d9d9fe4cfd2807336b9825ce3b449a64d47872
Cratters <4a2k0055@stust.edu.tw>
2017-11-30 23:39:46 +0800

    debug

--- app/src/main/java/ok/com/datetest/MainActivity.java
+++ app/src/main/java/ok/com/datetest/MainActivity.java
@@ -18,30 +18,30 @@ public class MainActivity extends AppCompatActivity {
     public static final String PREF_OLD_TIME = "DATE_OldTime";
     public static final String[] PREF_HASMOB = {"HasMob1","HasMob2","HasMob3","HasMob4","HasMob5","HasMob6"};
     public static final String DATE_NEXTDATE = "DATE_NextTime";
-    int timeGap,tCount;
+    int timeGap;
     long newTime,oldTime;
-    String tempOldTime;
+    String tempOldTime="0";
     String[] tempHasMod = new String[6];
     Calendar rightNow = Calendar.getInstance();
-
     TextView mob[];
-    //Boolean[] mobs = new Boolean[6];
 
     private void restorePrefs() { //讀取的位置
         SharedPreferences settings = getSharedPreferences(DATE_PREF, 0);
-        tempOldTime = settings.getString(PREF_OLD_TIME, "");
         for(int i=0; i<6; i++){
             tempHasMod[i] = settings.getString(PREF_HASMOB[i], "");
-            Log.d("LOGDATE_HasMod" + i, tempHasMod[i]);
         }
-        oldTime =  Long.parseLong(tempOldTime);
+        tempOldTime = settings.getString(PREF_OLD_TIME, "");
+        if(tempOldTime.equals("")) //玩家第一次開啟沒有舊時間// ，就將oldTime設置為now
+            oldTime = rightNow.getTimeInMillis();
+        else
+            oldTime = Long.parseLong(tempOldTime);
         Log.d("LOGDATE_NewTime", newTime+"");
         Log.d("LOGDATE_OldTime", oldTime+"");
     }
 
-    protected void onSave() { //儲存的位置
+    protected void onSave() { //儲存的位置，目前在onCreate最底下呼叫
         SharedPreferences settings = getSharedPreferences(DATE_PREF, 0);
-        settings.edit().putString(PREF_OLD_TIME, String.valueOf(newTime)).commit();
+        settings.edit().putString(PREF_OLD_TIME, String.valueOf(rightNow.getTimeInMillis())).commit();
         for(int i=0; i<6; i++){
             settings.edit().putString(PREF_HASMOB[i], tempHasMod[i]).commit();
         }
@@ -52,14 +52,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         newTime = rightNow.getTimeInMillis();
-        tempHasMod = new String[]{"0", "0", "0", "0", "0", "0"};
-        findViews();
+        tempHasMod = new String[]{"0", "0", "0", "0", "0", "0"}; //不知為何new在class外部會有問題所以先寫在這裡
         restorePrefs();
+        findViews();
         DateTest();
-        for(int i=0; i<tempHasMod.length; i++) {
-            Log.d("LOGDATE_HasMod" + i, tempHasMod[i]+",");}
         setVisible();
         onSave();
+    }
+
+    @Override
+    protected void onDestroy() {
+        onSave();
+        super.onDestroy();
     }
 
     void findViews() {
@@ -74,13 +78,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClear (View v) {
         for (int i=0;i<6;i++){
-            mob[i].setVisibility(View.GONE);
-            tCount = 0;
+            mob[i].setVisibility(View.INVISIBLE);
             tempHasMod[i] = "0";
         }
         onSave();
     }
-    int random;
+    int random,randSet;
     void DateTest () {
         timeGap = (int)(newTime-oldTime)/1000;
         Log.d("LOGDATE_timeGap", timeGap + "");
@@ -92,15 +95,23 @@ public class MainActivity extends AppCompatActivity {
                 if (timeGap < 0) {
                     break;
                 } else
-                    tempHasMod[i] = "1";
+                    while (true) {
+                        randSet = (int) (Math.random() * 6); //隨機 0-5
+                        Log.d("LOGDATE_RandSet:", randSet + "");
+                        if (tempHasMod[randSet].equals("0")) {
+                            tempHasMod[randSet] = "1";
+                            break;
+                        }
+                    }
             }
         }
     }
 
     void setVisible () {
+        Log.d("LOGDATE",tempHasMod[0]+tempHasMod[1]+tempHasMod[2]+tempHasMod[3]+tempHasMod[4]+tempHasMod[5]);
         for(int i=0; i<tempHasMod.length; i++) {
             if(tempHasMod[i].equals("0"))
-                mob[i].setVisibility(View.GONE);
+                mob[i].setVisibility(View.INVISIBLE);
             else
                 mob[i].setVisibility(View.VISIBLE);
         }
